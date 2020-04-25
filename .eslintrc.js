4cb8fafcb46ed063c7078d62d18e22af7e95fba3
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2020-04-25 01:47:29 -0400

    Updated project with vuejs working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    root: true,
+    env: {
+        node: true
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+    parserOptions: {
+        parser: "babel-eslint"
     },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-unused-vars": "off"
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
     }
 };
