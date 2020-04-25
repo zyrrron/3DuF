4f93658bd6a8078b36f09569d191322f185a34f7
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2020-04-25 05:21:57 -0400

    Its all working but its super buggy

--- src/components/Visualiser.vue
+++ src/components/Visualiser.vue
@@ -1,22 +1,26 @@
 <template>
-    <div>
-        <div id="view_container">
+        <!-- <div id="view_container">
             <div id="canvas_block">
                 <canvas id="c" tabindex="0" resize></canvas>
             </div>
             <div id="renderContainer"/>
+        </div> -->
+        <div id="view_container" >
+                <div id="canvas_block" class="shown-block fade-transition">
+                    <canvas id="c" tabindex="0" width="500" height="500" resize></canvas>
+                </div>
+                <div id="renderContainer" class="hidden-block fade-transition"></div>
         </div>
-    </div>
 </template>
 
 <script>
-import { Registry, BareViewManager } from "../app/index"
+import { Registry, ViewManager } from "../app/index"
 import { Examples } from "../app/index"
 
     export default {
-       mounted() {
+       mounted: function() {
 
-        let viewManager = new BareViewManager();
+        let viewManager = new ViewManager();
 
         Registry.viewManager = viewManager;
 
@@ -36,5 +40,6 @@ import { Examples } from "../app/index"
 #view-container {
     width: 100%;
     height: 100%;
+    position: relative;
 }
 </style>
